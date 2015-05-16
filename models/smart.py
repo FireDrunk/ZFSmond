0b662777c0a69291dada566c964d8f1845329ca5
Thijs Cramer <thijs.cramer@gmail.com>
2015-05-16 22:15:39 +0200

    - Added try/catch to capture pySMART errors

--- models/smart.py
+++ models/smart.py
@@ -3,9 +3,13 @@ from pySMART import Device
 
 class SmartInfo(restful.Resource):
     def get(self, disk):
+        try:
             raw_info = Device('/dev/' + disk)
             smart_info = {}
             for smart_value in raw_info.attributes:
                 if smart_value:
                     smart_info[smart_value.name] = smart_value.raw
             return smart_info
+        except:
+            print "[SMART] Unable to fetch SMART data for device: %s" % disk
+            return {"error": True}
