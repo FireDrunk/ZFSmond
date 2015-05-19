cdf9d6eb045c43508aeafd34c0bb62e014a75263
Thijs Cramer <thijs.cramer@gmail.com>
2015-05-19 15:27:17 +0200

    - Added custom filters for Scrub State and Scrub to Bootstrap Color

--- static/js/zfsmond.js
+++ static/js/zfsmond.js
@@ -1,4 +1,4 @@
-var app = angular.module('zfsmond', ['ui.bootstrap', 'ui.router','ngResource', 'zfsmond.pools','zfsmond.filesystems', 'zfsmond.disks']);
+var app = angular.module('zfsmond', ['ui.bootstrap', 'ui.router','ngResource', 'zfsmond.pools','zfsmond.filesystems', 'zfsmond.disks', 'zfsmond.stats']);
 
 app.config(function($stateProvider, $urlRouterProvider) {
 	$urlRouterProvider.otherwise("/pools")
@@ -45,5 +45,48 @@ app.filter('bytes_1000', function() {
 		var units = ['bytes', 'kB', 'MB', 'GB', 'TB', 'PB'],
 			number = Math.floor(Math.log(bytes) / Math.log(1024));
 		return (bytes / Math.pow(1000, Math.floor(number))).toFixed(precision) +  ' ' + units[number];
+	}
+});
+
+app.filter('scrub_state_to_text', function() {
+	return function(state){
+		switch(state) {
+			case 2: return "Finished";
+			case 1: return "Running";
+			default: return "Unkown"
+		}
+	}
+});
+
+app.filter('scrub_state_to_class', function() {
+	return function(state) {
+			switch (state) {
+				case 2: return "success";
+				case 1: return "warning";
+				default: return "default";
+			}
+	}
+});
+
+app.filter('time_to_text', function() {
+	return function(time) {
+    var minutes = time / 60;
+    secs = Math.floor(time % 60);
+    var hours = minutes / 60;
+    minutes = Math.floor(minutes % 60);
+    hours = Math.floor(hours % 24);
+		if (hours < 10)
+			var real_hours = "0" + hours;
+		else
+			var real_hours = hours;
+		if (minutes < 10)
+			var real_minutes = "0" + minutes;
+		else
+				var real_minutes = minutes;
+		if (secs < 10)
+			var real_secs = "0" + secs
+		else
+			var real_secs = secs;
+    return real_hours + ":" + real_minutes + ":" + real_secs;
 	}
 });
