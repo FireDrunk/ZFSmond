ecdf381c345c795bca3567e34e8f385136bef435
Thijs Cramer <thijs.cramer@gmail.com>
2015-05-19 22:34:12 +0200

    - Added 1 second auto-refresh

--- static/controllers/stats.js
+++ static/controllers/stats.js
@@ -1,15 +1,23 @@
 angular.module('zfsmond.stats',[])
-	.controller('StatsController', ['$scope','$http',function($scope, $http)
+	.controller('StatsController', ['$scope','$http','$interval',function($scope, $http, $interval)
   {
     var statsCtrl = this
 		statsCtrl.stats = []
 
-		$http.get('/api/stats').
-			success(function(data, status, headers, config)
-			{
-        statsCtrl.stats = data
-			}).
-			error(function(data, status, headers, config) {
-				console.log('Error!')
-			});
+		var auto_update_stats = $interval(function () {
+			$http.get('/api/stats').
+				success(function(data, status, headers, config)
+				{
+	        statsCtrl.stats = data
+				}).
+				error(function(data, status, headers, config) {
+					console.log('Error!')
+				});
+		}, 1000);
+
+		// listen on DOM destroy (removal) event, and cancel the next UI update
+		// to prevent updating time after the DOM element was removed.
+		$scope.$on("$destroy", function() {
+			$interval.cancel(auto_update_stats);
+		});
 	}]);
