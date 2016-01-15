b49ce44e1b718ddca916b498fdfdb44f797b5f2c
Janne Heß <jannehess@gmail.com>
2016-01-15 21:28:47 +0100

    Automatically reload disks

--- static/controllers/disks.js
+++ static/controllers/disks.js
@@ -1,21 +1,28 @@
 angular.module('zfsmond.disks',[])
-	.controller('DisksController',['$scope','$http', function($scope, $http) {
+	.controller('DisksController',['$scope','$http','$interval',function($scope, $http, $interval) {
 		$scope.disks = [];
 		$scope.smartinfo = [];
-		//$scope.http = $http;
 
-		$http.get('/api/disks').
-			success(function(data, status, headers, config)
-			{
-				$scope.disks = data;
-			}).
-			error(function(data, status, headers, config) {
-				console.log('Error retreiving disk information from API!');
-			});
+		var disks_updater = function() {
+			$http.get('/api/disks').
+				success(function(data, status, headers, config)
+				{
+					$scope.disks = data
+				}).
+				error(function(data, status, headers, config)
+				{
+					console.log('Could not fetch updates!')
+				});
+		};
+		var disks_interval = $interval(disks_updater, 1000);
+		disks_updater();
+
+		$scope.$on('$destroy', function() {
+			$interval.cancel(disks_interval);
+		});
+
 		var http = $http;
-		
 		$scope.getSmart = function(dev) {
-			console.log("Smart button pressed! (" + dev +")");
 			http.get('/api/smart/' + dev).success(function(smart_response){
 				$scope.smartinfo[dev] = smart_response;
 				console.log("SMART Info: " + smart_response);
