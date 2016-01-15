f3dd2bb1d04eb6448864840cf2ca374c5a9fe445
Janne Heß <jannehess@gmail.com>
2016-01-15 21:33:27 +0100

    Automatically reload stats immediately

--- static/controllers/stats.js
+++ static/controllers/stats.js
@@ -1,23 +1,23 @@
 angular.module('zfsmond.stats',[])
-	.controller('StatsController', ['$scope','$http','$interval',function($scope, $http, $interval)
-  {
-    var statsCtrl = this
-		statsCtrl.stats = []
+	.controller('StatsController', ['$scope','$http','$interval',function($scope, $http, $interval) {
+		$scope.stats = []
 
-		var auto_update_stats = $interval(function () {
+		var stats_updater = function() {
 			$http.get('/api/stats').
 				success(function(data, status, headers, config)
 				{
-	        statsCtrl.stats = data
+					$scope.stats = data
 				}).
 				error(function(data, status, headers, config) {
-					console.log('Error!')
+					console.log('Could not fetch updates!')
 				});
-		}, 1000);
+		};
+		var stats_interval = $interval(stats_updater, 1000);
+		stats_updater();
 
 		// listen on DOM destroy (removal) event, and cancel the next UI update
 		// to prevent updating time after the DOM element was removed.
-		$scope.$on("$destroy", function() {
-			$interval.cancel(auto_update_stats);
+		$scope.$on('$destroy', function() {
+			$interval.cancel(stats_interval);
 		});
 	}]);
